0ced72a96dd593a326d4fb79b29d5f4e6d107341
Jakov Glavaš <jglavas@Jakovs-MacBook-Pro-2.fritz.box>
2022-06-01 15:30:39 +0200

    feat: Initial commit

---
+++ src/main/java/com/upworkscraper/upworkscraper/UpworkScraperApplication.java
@@ -0,0 +1,13 @@
+package com.upworkscraper.upworkscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpworkScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpworkScraperApplication.class, args);
+	}
+
+}
